18ac9da0ceb04aa0d7cf455d778a34577fe34a6b
Alisson Barros <50027706+alibarros94@users.noreply.github.com>
2025-05-11 17:57:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { DOMParser } = require('xmldom');
+const { DOMParser } = require('@xmldom/xmldom');
 const { SignedXml } = require('xml-crypto');
 
 const app = express();
